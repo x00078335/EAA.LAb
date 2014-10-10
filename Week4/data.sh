281641737ad73db681f004804202084f37e51a89
mint <x00078335>
2014-10-10 11:56:51 +0100

    final week 4

---
+++ Week4/data.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+#: Title : question 5
+#: Date : 10/10/2014
+#: Author : nathan judge
+#: Version :
+#: Description : 
+#: Options : 
+
+echo "Please enter your username: "
+read input_variable1
+
+
+echo "Please enter the month you were born: "
+read input_variable2
+
+
+echo "Please enter your favourite colour: "
+read input_variable3
+
+
+echo “Your Name is: $input_variable1. You were born in $input_variable2 and your favourite colour is $input_variable3”
---
+++ Week4/scripts/diary.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+#: Title : Question 6
+#: Date : 10/10/14
+#: Author : Nathan
+#: Version :
+#: Description : 
+#: Options : 
+
+
+
+echo "How do you feel?"
+read input_variable1
+
+echo input_variable1 >> diary.txt
+
+date >> diary.txt 
